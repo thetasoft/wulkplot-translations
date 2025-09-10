8302d41106163a670cabf59bb89c017252ba66ce
Alyner3 <qualienhd@gmail.com>
2025-09-10 19:50:09 +0200

    meta: update .ts metadata from sources

--- wulkplot_fr.ts
+++ wulkplot_fr.ts
@@ -1,169 +1,169 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR" sourcelanguage="en_US">
-<context>
+  <context>
     <name>ColumnSelectionDialog</name>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="14"/>
-        <source>Select columns</source>
-        <translation>Colonnes sélectionnées</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="20" />
+      <source>Select columns</source>
+      <translation>Colonnes sélectionnées</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="24"/>
-        <source>Add</source>
-        <translation>Ajouter</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="30" />
+      <source>Add</source>
+      <translation>Ajouter</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="25"/>
-        <source>Cancel</source>
-        <translation>Annuler</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="31" />
+      <source>Cancel</source>
+      <translation>Annuler</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ConsoleWindow</name>
     <message>
-        <location filename="../util_qt/__advanced/console_window.py" line="11"/>
-        <source>Console</source>
-        <translation>Console</translation>
+      <location filename="..\src\wulkplot\util_qt\__advanced\console_window.py" line="12" />
+      <source>Console</source>
+      <translation>Console</translation>
     </message>
     <message>
-        <location filename="../util_qt/__advanced/console_window.py" line="15"/>
-        <source>Time</source>
-        <translation>Temps</translation>
+      <location filename="..\src\wulkplot\util_qt\__advanced\console_window.py" line="17" />
+      <source>Time</source>
+      <translation>Temps</translation>
     </message>
     <message>
-        <location filename="../util_qt/__advanced/console_window.py" line="15"/>
-        <source>PID</source>
-        <translation>IDP</translation>
+      <location filename="..\src\wulkplot\util_qt\__advanced\console_window.py" line="17" />
+      <source>PID</source>
+      <translation>IDP</translation>
     </message>
     <message>
-        <location filename="../util_qt/__advanced/console_window.py" line="15"/>
-        <source>Output</source>
-        <translation>Sortie</translation>
+      <location filename="..\src\wulkplot\util_qt\__advanced\console_window.py" line="17" />
+      <source>Output</source>
+      <translation>Sortie</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>DataFileSelectionDialog</name>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="59"/>
-        <source>Add data files</source>
-        <translation>Ajouter des fichiers de données</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="69" />
+      <source>Add data files</source>
+      <translation>Ajouter des fichiers de données</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="66"/>
-        <source>Add</source>
-        <translation>Ajouter</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="76" />
+      <source>Add</source>
+      <translation>Ajouter</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="67"/>
-        <source>Cancel</source>
-        <translation>Annuler</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="77" />
+      <source>Cancel</source>
+      <translation>Annuler</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="75"/>
-        <source>Files</source>
-        <translation>Fichiers</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="85" />
+      <source>Files</source>
+      <translation>Fichiers</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ExperimentGUI</name>
     <message>
-        <location filename="../gui.py" line="76"/>
-        <source>Add files</source>
-        <translation>Ajouter des données</translation>
+      <location filename="..\src\wulkplot\gui.py" line="85" />
+      <source>Add files</source>
+      <translation>Ajouter des données</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="81"/>
-        <source>Run script</source>
-        <translation>Exécuter</translation>
+      <location filename="..\src\wulkplot\gui.py" line="90" />
+      <source>Run script</source>
+      <translation>Exécuter</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="87"/>
-        <source>Script parameters</source>
-        <translation>Paramètres du script</translation>
+      <location filename="..\src\wulkplot\gui.py" line="96" />
+      <source>Script parameters</source>
+      <translation>Paramètres du script</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="116"/>
-        <source>Scripts</source>
-        <translation>Scripts</translation>
+      <location filename="..\src\wulkplot\gui.py" line="126" />
+      <source>Scripts</source>
+      <translation>Scripts</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="130"/>
-        <source>Script</source>
-        <translation>Script</translation>
+      <location filename="..\src\wulkplot\gui.py" line="141" />
+      <source>Script</source>
+      <translation>Script</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="130"/>
-        <source>PID</source>
-        <translation>IDP</translation>
+      <location filename="..\src\wulkplot\gui.py" line="141" />
+      <source>PID</source>
+      <translation>IDP</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="130"/>
-        <source>Action</source>
-        <translation>Action</translation>
+      <location filename="..\src\wulkplot\gui.py" line="141" />
+      <source>Action</source>
+      <translation>Action</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="214"/>
-        <source>Failed to read CSV file:</source>
-        <translation>Échec de la lecture du fichier CSV :</translation>
+      <location filename="..\src\wulkplot\gui.py" line="220" />
+      <source>Failed to read CSV file:</source>
+      <translation>Échec de la lecture du fichier CSV :</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="223"/>
-        <source>No columns selected</source>
-        <translation>Aucune colonne sélectionnée</translation>
+      <location filename="..\src\wulkplot\gui.py" line="230" />
+      <source>No columns selected</source>
+      <translation>Aucune colonne sélectionnée</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="315"/>
-        <source>builtin</source>
-        <translation>intégré</translation>
+      <location filename="..\src\wulkplot\gui.py" line="293" />
+      <source>builtin</source>
+      <translation>intégré</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="383"/>
-        <source>No script selected</source>
-        <translation>Aucun script sélectionné</translation>
+      <location filename="..\src\wulkplot\gui.py" line="360" />
+      <source>No script selected</source>
+      <translation>Aucun script sélectionné</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="402"/>
-        <source>No data files selected</source>
-        <translation>Aucun fichier de données sélectionné</translation>
+      <location filename="..\src\wulkplot\gui.py" line="379" />
+      <source>No data files selected</source>
+      <translation>Aucun fichier de données sélectionné</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="437"/>
-        <source>Failed to start script:</source>
-        <translation>Échec du démarrage du script :</translation>
+      <location filename="..\src\wulkplot\gui.py" line="426" />
+      <source>Failed to start script:</source>
+      <translation>Échec du démarrage du script :</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="474"/>
-        <source>Kill</source>
-        <translation>Arrêter</translation>
+      <location filename="..\src\wulkplot\gui.py" line="447" />
+      <source>Kill</source>
+      <translation>Arrêter</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="493"/>
-        <source>Failed to kill process:</source>
-        <translation>Échec de l&apos;arrêt du processus :</translation>
+      <location filename="..\src\wulkplot\gui.py" line="469" />
+      <source>Failed to kill process:</source>
+      <translation>Échec de l'arrêt du processus :</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="604"/>
-        <source>Failed to load script:</source>
-        <translation>Échec du chargement du script :</translation>
+      <location filename="..\src\wulkplot\gui.py" line="577" />
+      <source>Failed to load script:</source>
+      <translation>Échec du chargement du script :</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SelectedFilesWidget</name>
     <message>
-        <location filename="../util_qt/__generic/qt_widgets.py" line="13"/>
-        <source>File</source>
-        <translation>Fichier</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_widgets.py" line="24" />
+      <source>File</source>
+      <translation>Fichier</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_widgets.py" line="13"/>
-        <source>Columns</source>
-        <translation>Colonnes</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_widgets.py" line="24" />
+      <source>Columns</source>
+      <translation>Colonnes</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_widgets.py" line="13"/>
-        <source>Actions</source>
-        <translation>Actions</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_widgets.py" line="24" />
+      <source>Actions</source>
+      <translation>Actions</translation>
     </message>
-</context>
+  </context>
 </TS>
--- wulkplot_no.ts
+++ wulkplot_no.ts
@@ -1,169 +1,169 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="nb_NO" sourcelanguage="en_US">
-<context>
+  <context>
     <name>ColumnSelectionDialog</name>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="14"/>
-        <source>Select columns</source>
-        <translation>Velg kolonner</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="20" />
+      <source>Select columns</source>
+      <translation>Velg kolonner</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="24"/>
-        <source>Add</source>
-        <translation>Legg til</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="30" />
+      <source>Add</source>
+      <translation>Legg til</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="25"/>
-        <source>Cancel</source>
-        <translation>Avbryt</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="31" />
+      <source>Cancel</source>
+      <translation>Avbryt</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ConsoleWindow</name>
     <message>
-        <location filename="../util_qt/__advanced/console_window.py" line="11"/>
-        <source>Console</source>
-        <translation>Konsoll</translation>
+      <location filename="..\src\wulkplot\util_qt\__advanced\console_window.py" line="12" />
+      <source>Console</source>
+      <translation>Konsoll</translation>
     </message>
     <message>
-        <location filename="../util_qt/__advanced/console_window.py" line="15"/>
-        <source>Time</source>
-        <translation>Tid</translation>
+      <location filename="..\src\wulkplot\util_qt\__advanced\console_window.py" line="17" />
+      <source>Time</source>
+      <translation>Tid</translation>
     </message>
     <message>
-        <location filename="../util_qt/__advanced/console_window.py" line="15"/>
-        <source>PID</source>
-        <translation>PID</translation>
+      <location filename="..\src\wulkplot\util_qt\__advanced\console_window.py" line="17" />
+      <source>PID</source>
+      <translation>PID</translation>
     </message>
     <message>
-        <location filename="../util_qt/__advanced/console_window.py" line="15"/>
-        <source>Output</source>
-        <translation>Utdata</translation>
+      <location filename="..\src\wulkplot\util_qt\__advanced\console_window.py" line="17" />
+      <source>Output</source>
+      <translation>Utdata</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>DataFileSelectionDialog</name>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="59"/>
-        <source>Add data files</source>
-        <translation>Legg til datafiler</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="69" />
+      <source>Add data files</source>
+      <translation>Legg til datafiler</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="66"/>
-        <source>Add</source>
-        <translation>Legg til</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="76" />
+      <source>Add</source>
+      <translation>Legg til</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="67"/>
-        <source>Cancel</source>
-        <translation>Avbryt</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="77" />
+      <source>Cancel</source>
+      <translation>Avbryt</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_dialogs.py" line="75"/>
-        <source>Files</source>
-        <translation>Filer</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_dialogs.py" line="85" />
+      <source>Files</source>
+      <translation>Filer</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ExperimentGUI</name>
     <message>
-        <location filename="../gui.py" line="76"/>
-        <source>Add files</source>
-        <translation>Legg til filer</translation>
+      <location filename="..\src\wulkplot\gui.py" line="85" />
+      <source>Add files</source>
+      <translation>Legg til filer</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="81"/>
-        <source>Run script</source>
-        <translation>Kjør skript</translation>
+      <location filename="..\src\wulkplot\gui.py" line="90" />
+      <source>Run script</source>
+      <translation>Kjør skript</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="87"/>
-        <source>Script parameters</source>
-        <translation>Skriptparametere</translation>
+      <location filename="..\src\wulkplot\gui.py" line="96" />
+      <source>Script parameters</source>
+      <translation>Skriptparametere</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="116"/>
-        <source>Scripts</source>
-        <translation>Skript</translation>
+      <location filename="..\src\wulkplot\gui.py" line="126" />
+      <source>Scripts</source>
+      <translation>Skript</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="130"/>
-        <source>Script</source>
-        <translation>Skript</translation>
+      <location filename="..\src\wulkplot\gui.py" line="141" />
+      <source>Script</source>
+      <translation>Skript</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="130"/>
-        <source>PID</source>
-        <translation>PID</translation>
+      <location filename="..\src\wulkplot\gui.py" line="141" />
+      <source>PID</source>
+      <translation>PID</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="130"/>
-        <source>Action</source>
-        <translation>Handling</translation>
+      <location filename="..\src\wulkplot\gui.py" line="141" />
+      <source>Action</source>
+      <translation>Handling</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="214"/>
-        <source>Failed to read CSV file:</source>
-        <translation>Klarte ikke å lese den CSV-filen:</translation>
+      <location filename="..\src\wulkplot\gui.py" line="220" />
+      <source>Failed to read CSV file:</source>
+      <translation>Klarte ikke å lese den CSV-filen:</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="223"/>
-        <source>No columns selected</source>
-        <translation>Ingen kolonner valgt</translation>
+      <location filename="..\src\wulkplot\gui.py" line="230" />
+      <source>No columns selected</source>
+      <translation>Ingen kolonner valgt</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="315"/>
-        <source>builtin</source>
-        <translation>innebygd</translation>
+      <location filename="..\src\wulkplot\gui.py" line="293" />
+      <source>builtin</source>
+      <translation>innebygd</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="383"/>
-        <source>No script selected</source>
-        <translation>Ingen skript valgt</translation>
+      <location filename="..\src\wulkplot\gui.py" line="360" />
+      <source>No script selected</source>
+      <translation>Ingen skript valgt</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="402"/>
-        <source>No data files selected</source>
-        <translation>Ingen datafiler valgt</translation>
+      <location filename="..\src\wulkplot\gui.py" line="379" />
+      <source>No data files selected</source>
+      <translation>Ingen datafiler valgt</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="437"/>
-        <source>Failed to start script:</source>
-        <translation>Klarte ikke å starte skriptet:</translation>
+      <location filename="..\src\wulkplot\gui.py" line="426" />
+      <source>Failed to start script:</source>
+      <translation>Klarte ikke å starte skriptet:</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="474"/>
-        <source>Kill</source>
-        <translation>Avslutt</translation>
+      <location filename="..\src\wulkplot\gui.py" line="447" />
+      <source>Kill</source>
+      <translation>Avslutt</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="493"/>
-        <source>Failed to kill process:</source>
-        <translation>Klarte ikke å avslutte prosessen:</translation>
+      <location filename="..\src\wulkplot\gui.py" line="469" />
+      <source>Failed to kill process:</source>
+      <translation>Klarte ikke å avslutte prosessen:</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="604"/>
-        <source>Failed to load script:</source>
-        <translation>Klarte ikke å laste inn skriptet:</translation>
+      <location filename="..\src\wulkplot\gui.py" line="577" />
+      <source>Failed to load script:</source>
+      <translation>Klarte ikke å laste inn skriptet:</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SelectedFilesWidget</name>
     <message>
-        <location filename="../util_qt/__generic/qt_widgets.py" line="13"/>
-        <source>File</source>
-        <translation>Fil</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_widgets.py" line="24" />
+      <source>File</source>
+      <translation>Fil</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_widgets.py" line="13"/>
-        <source>Columns</source>
-        <translation>Kolonner</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_widgets.py" line="24" />
+      <source>Columns</source>
+      <translation>Kolonner</translation>
     </message>
     <message>
-        <location filename="../util_qt/__generic/qt_widgets.py" line="13"/>
-        <source>Actions</source>
-        <translation>Handlinger</translation>
+      <location filename="..\src\wulkplot\util_qt\__generic\qt_widgets.py" line="24" />
+      <source>Actions</source>
+      <translation>Handlinger</translation>
     </message>
-</context>
+  </context>
 </TS>
